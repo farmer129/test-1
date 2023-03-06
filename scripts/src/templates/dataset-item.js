5b459f34a681411302fcac261ec51d2320c4cc70
Frank Chen <99700203+FrankChen2023@users.noreply.github.com>
2023-03-06 09:59:53 +0000

    Update dataset-item.js

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,6 +1,12 @@
-export default (data) => (
-`<dataset>
+import clip from "text-clipper";
+
+export default (data) => {
+  const resources = [... new Set(data.resources.map(function (res) { return `<span class="badge filetype-list-item">${res.format.toUpperCase()}</span>` }))].join('');
+  const clippedHtml = clip(data.notes, 450, { html: true, maxLines: 1 });
+
+  return `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
-  ${data.notes || ''}
+  <div class="dataset-item-description">${clippedHtml || ''}</div>
+  <div>${resources}</div>
 </dataset>`
-)
+}
